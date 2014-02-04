58b890b2187b5cecbf2114f666444d0f4669c996
Eric Jones <eric.web.email@gmail.com>
2014-02-03 20:26:22 -0500

    Removed the Category Seeder from the project start branch.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder {
 		// $this->call('UserTableSeeder');
 		$this->call('PostsTableSeeder');
 		$this->call('UsersTableSeeder');
-		$this->call('CategoriesTableSeeder');
 	}
 
 }
